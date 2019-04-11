5bb8cd9a688cbf4f60a57aedb7c6bb47bd775da3
Anniken Bråthen <post.anniken@live.no>
2019-04-11 09:11:25 +0200

    detaljer

--- javascript/java.js
+++ javascript/java.js
@@ -1,4 +1,11 @@
-const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
+
+
+const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json";
+
+function func() {
+   document.getElementById('accountDetails').hidden = "";
+}
+
 const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
