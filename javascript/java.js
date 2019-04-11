d6447015569dce3850d9dbacb7489055c8c508aa
idaod <48239091+idaod@users.noreply.github.com>
2019-04-11 09:21:51 +0200

    .

--- javascript/java.js
+++ javascript/java.js
@@ -1,6 +1,19 @@
-const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
-const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
-const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
+//const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
+//const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
+//const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
 
-console.log("hei")
+function oversikt() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var variabel = JSON.parse(this.responseText);
+      document.getElementById("hello").innerHTML = variabel;
+    }
+  };
+  xhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
+  xhttp.send();
+}
+
+
+console.log("hei");
