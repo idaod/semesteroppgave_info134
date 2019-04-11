f5b7b3bfd2badd96360ec4edb632e0dfcf5da77e
Anniken Bråthen <post.anniken@live.no>
2019-04-11 09:20:30 +0200

    java

--- javascript/java.js
+++ javascript/java.js
@@ -1,13 +1,31 @@
 
 
 const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json";
-
-function func() {
-   document.getElementById('accountDetails').hidden = "";
-}
-
 const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
 
 console.log("hei")
+
+
+
+//DETALJER
+//komunenavn, nr, siste målte befolkning(2018), siste målte statestikk for sysselsetting og utdanning antall + %.
+//Historisk utvikling i tabell/graf
+function detaljer() {
+  var komunenummer = "nummer";
+
+  var xhr = new XMLHttpRequest();
+  var URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json";
+  xhr.open("GET", URL_befolkning);
+  xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    representResult(JSON.parse(xhr.responseText));
+
+  }
+};
+xmlhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
+xmlhttp.send();
+}
+
+detaljer();
