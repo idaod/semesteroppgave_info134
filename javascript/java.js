188b45eb43e867e2702c4388e9c686d6610920dc
idaod <48239091+idaod@users.noreply.github.com>
2019-04-11 11:13:31 +0200

    .

--- javascript/java.js
+++ javascript/java.js
@@ -1,75 +1,46 @@
 
-
-//const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
-//const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
-//const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
-
-
-function oversikt() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var kommune = JSON.parse(this.responseText);
-      document.getElementById("hello").innerHTML = kommune;
-    }
-  };
-  xhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
-  xhttp.send();
-}
-
-
-
 const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json";
 const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
 const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
 
 
-console.log("hei")
-
-
-
-//DETALJER
-//komunenavn, nr, siste målte befolkning(2018), siste målte statestikk for sysselsetting og utdanning antall + %.
-//Historisk utvikling i tabell/graf
-function detaljer() {
-  var komunenummer = "nummer";
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var variabel = JSON.parse(this.responseText);
-    document.getElementById("hello").innerHTML = variabel;
-
-
-  }
-};
-xmlhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
-xmlhttp.send();
-}
-
-detaljer();
-
-
-
-//const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
-//const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
-//const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
-
-
-
-function oversikt(url, objekt) {
+function parset_tekst(url, objekt) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var variabel = JSON.parse(this.responseText);
       objekt.data = variabel
+      console.lot(objekt.data);
     }
   };
   xhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
   xhttp.send();
 }
 
-function befolkning(url){
+
+function befolkning(URL_befolkning){
+  this.getNames = function(){
+    
+    }
+
+  }
+
+befolkning();
+
+
   this.load = function(){
-    oversikt(url, this)
+    parset_tekst(url, this);
+  }
+}
+
+function sysselsetting(URL_sysselsetting){
+  this.load = function(){
+    oversikt(url, this);
+  }
+}
+
+function utdanning(URL_utdanning){
+  this.load = function(){
+    oversikt(url, this);
   }
 }
