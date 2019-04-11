cac88d0082798036d2c4df53ffd29d691bf2032a
Anniken Bråthen <post.anniken@live.no>
2019-04-11 09:28:41 +0200

    .

--- javascript/java.js
+++ javascript/java.js
@@ -1,3 +1,22 @@
+//const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
+//const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
+//const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
+
+
+function oversikt() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var kommune = JSON.parse(this.responseText);
+      document.getElementById("hello").innerHTML = kommune;
+    }
+  };
+  xhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
+  xhttp.send();
+}
+
+
+console.log("hei");
 <<<<<<< HEAD
 
 
@@ -15,13 +34,12 @@ console.log("hei")
 //Historisk utvikling i tabell/graf
 function detaljer() {
   var komunenummer = "nummer";
-
   var xhr = new XMLHttpRequest();
-  var URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json";
-  xhr.open("GET", URL_befolkning);
   xhr.onreadystatechange = function () {
   if (xhr.readyState === 4 && xhr.status === 200) {
-    representResult(JSON.parse(xhr.responseText));
+    var variabel = JSON.parse(this.responseText);
+    document.getElementById("hello").innerHTML = variabel;
+
 
   }
 };
@@ -30,24 +48,3 @@ xmlhttp.send();
 }
 
 detaljer();
-=======
-//const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json"
-//const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
-//const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
-
-
-function oversikt() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var variabel = JSON.parse(this.responseText);
-      document.getElementById("hello").innerHTML = variabel;
-    }
-  };
-  xhttp.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
-  xhttp.send();
-}
-
-
-console.log("hei");
->>>>>>> 31ab5bb7fcde3a823bf0f2f30021fcc3fbb7a454
