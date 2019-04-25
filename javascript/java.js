db9312dd7fd70a09fa081aa0faa8b3cb9adf3974
idaod <48239091+idaod@users.noreply.github.com>
2019-04-25 09:14:49 +0200

    .

--- javascript/java.js
+++ javascript/java.js
@@ -1,37 +1,53 @@
+let befolkning;
+let sysselsetting;
+let utdanning;
+
+window.onload = function(){
+   befolkning = new Befolkning(URL_befolkning);
+   sysselsetting = new Sysselsetting(URL_sysselsetting);
+   utdanning = new Utdanning(URL_utdanning);
+}
 
 const URL_befolkning = "http://wildboy.uib.no/~tpe056/folk/104857.json";
-const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json"
-const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json"
+const URL_sysselsetting = "http://wildboy.uib.no/~tpe056/folk/100145.json";
+const URL_utdanning = "http://wildboy.uib.no/~tpe056/folk/85432.json";
 
 
 function parset_tekst(url, objekt) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+    console.log(this.status)
     if (this.readyState == 4 && this.status == 200) {
       var variabel = JSON.parse(this.responseText);
-      console.log(variabel);
       objekt.data = variabel
     }
   };
   xhttp.open("GET", url);
-  xhttp.send();
+  xhttp.send(null);
 }
 
-parset_tekst(URL_utdanning);
-
-
-
-
-let befolkning = new Befolkning(URL_befolkning,this)
-
-befolkning.load()
 
 function Befolkning(url){
-  this.getNames = getNames
-  this.getIDs = getIDs
-  this.getInfo = getInfo
-  this.load = parset_tekst
+  //this.getNames = getNames
+  //this.getIDs = getIDs
+  //this.getInfo = getInfo
+  this.load = function(){
+    parset_tekst(url,this)
+  }
 }
+
+//function getNames(){
+//  this.data???
+
+
+function Sysselsetting(url){
+  //this.getNames = getNames
+  //this.getIDs = getIDs
+  //this.getInfo = getInfo
+  this.load = parset_tekst
+
+}
+
 
 
 
